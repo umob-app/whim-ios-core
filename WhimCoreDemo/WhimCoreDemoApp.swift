92a28237c56127ec5992a7f2a9bb2cb6667d37f6
Duc Do <duc.fet01@gmail.com>
2022-10-03 16:54:09 +0300

    Fix test from core lib

---
+++ WhimCoreDemo/WhimCoreDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhimCoreDemoApp.swift
+//  WhimCoreDemo
+//
+//  Created by Duc Do on 29.9.2022.
+//
+
+import SwiftUI
+
+@main
+struct WhimCoreDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
