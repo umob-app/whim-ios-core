98b7768e668eaf7a1a90f16967edaa1591ea43bd
Duc Do <duc.fet01@gmail.com>
2022-10-28 09:27:57 +0300

    Add example project

---
+++ WhimCoreExample/WhimCoreExample/WhimCoreExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhimCoreExampleApp.swift
+//  WhimCoreExample
+//
+//  Created by Duc Do on 27.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct WhimCoreExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
